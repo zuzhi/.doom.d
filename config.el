7967baf21e125fddf42900b867499ceac49bc21b
zuzhi <zuzhi.hu@gmail.com>
2025-02-18 11:23:24 +0800

    reset and update config files

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "zuzhi"
-      user-mail-address "zuzhi.hu@gmail.com")
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -15,7 +15,7 @@
 ;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
 ;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-symbol-font' -- for symbols
 ;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
 ;; See 'C-h v doom-font' for documentation and more examples of what they
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-nord)
+;;(setq doom-theme 'doom-nord-aurora)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -75,260 +75,40 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+(setq user-full-name "zuzhi"
+      user-mail-address "zuzhi.hu@gmail.com")
 
-;; ------------------------------------------------------------------------------
-;;
-;; Frame
 ;; Maximize Emacs frame on start-up
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-;;
-;; Org
-;; Add a timestamp when a 'TODO' item closed.
-(setq org-log-done 'time)
-
-;; File watchers
-;; Increase the file watch warning threshold, the default is 1000
-(setq lsp-file-watch-threshold 4000)
-
-;; JDL Mode
-;; Load the package
-(load "jdl-mode")
-
-;; PDF Tools
-;; Prerequisite: https://github.com/politza/pdf-tools#compiling-on-os-x
-;; Activate pdf-tools
-;;(pdf-loader-install) ;; FIXME void-function error
-;; Uses more memory; see https://github.com/politza/pdf-tools/issues/51
-;;(setq pdf-view-use-scaling t
-;;      pdf-view-use-imagemagick nil)
-
-;; Org todo
-(defun org-archive-done-tasks ()
-  (interactive)
-  (org-map-entries
-   (lambda ()
-     (org-archive-subtree)
-     (setq org-map-continue-from (org-element-property :begin (org-element-at-point))))
-   "/DONE" 'tree))
-
-;; Camel case for yasnippets
-(defun camelcase (string)
-  (concat (downcase (substring string 0 1)) (substring string 1)))
-
-;; Horizontal recenter
-;; https://stackoverflow.com/questions/1249497/command-to-center-screen-horizontally-around-cursor-on-emacs
-(defun my-horizontal-recenter ()
-  "make the point horizontally centered in the window"
-  (interactive)
-  (let ((mid (/ (window-width) 2))
-        (cur (current-column)))
-    (if (< mid cur)
-        (set-window-hscroll (selected-window)
-                            (- cur mid)))))
-(global-set-key (kbd "C-S-l") 'my-horizontal-recenter)
-
-;; Set eshell aliases
-(set-eshell-alias! "up" "eshell-up $1")
-
 ;; Fonts
-(setq doom-font (font-spec :family "IBM Plex Mono" :size 14 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "IBM Plex Mono" :size 14))
+;(setq doom-font (font-spec :family "IBM Plex Mono" :size 14 :weight 'regular)
+;      doom-variable-pitch-font (font-spec :family "IBM Plex Mono" :size 14))
 
-;; https://github.com/Alexander-Miller/treemacs/issues/626#issuecomment-707471200
-;; try to fix treemacs, doesn't work though
-(setq lsp-enable-links nil
-      treemacs-follow-mode nil)
+(setq doom-theme 'doom-nord
+      doom-font (font-spec :family "JetBrains Mono" :size 12)
+      doom-variable-pitch-font (font-spec :family "DejaVu Sans" :size 13))
+
+(setq nerd-icons-font-names '("SymbolsNerdFontMono-Regular.ttf"))
+
+;; Line numbers are pretty slow all around. The performance boost of disabling
+;; them outweighs the utility of always keeping them on.
+(setq display-line-numbers-type nil)
 
 ;; accept completion from copilot and fallback to company
-;;(use-package! copilot
-;;  :hook (prog-mode . copilot-mode)
-;;  :bind (("C-TAB" . 'copilot-accept-completion-by-word)
-;;         ("C-<tab>" . 'copilot-accept-completion-by-word)
-;;         :map copilot-completion-map
-;;         ("<tab>" . 'copilot-accept-completion)
-;;         ("TAB" . 'copilot-accept-completion)))
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)
+              ("C-n" . 'copilot-next-completion)
+              ("C-p" . 'copilot-previous-completion))
 
-
-;; use javascript-mode for .mjs files, typescript-mode for .mts files
-(add-to-list 'auto-mode-alist '("\\.mjs\\'" . typescript-mode))
-(add-to-list 'auto-mode-alist '("\\.mts\\'" . typescript-mode))
-
-;;
-(defun insert-current-date-time () (interactive)
-       (insert (shell-command-to-string "echo -n $(date +%Y%m%d%H%M%S)")))
-
-(defun insert-current-date () (interactive)
-       (insert (shell-command-to-string "echo -n $(date +%Y%m%d)")))
-
-(after! chatgpt-shell
-  (setq chatgpt-shell-openai-key (getenv "OPENAI_API_KEY")))
-
-;;
-(defun copy-current-line-position-to-clipboard ()
-  "Copy current line in file to clipboard as '</path/to/file>:<line-number>'"
-  (interactive)
-  (let ((path-with-line-number
-         (concat (buffer-file-name) ":" (number-to-string (line-number-at-pos)))))
-    (x-select-text path-with-line-number)
-    (message (concat path-with-line-number " copied to clipboard"))))
-
-(define-key global-map (kbd "M-l") 'copy-current-line-position-to-clipboard)
-
-(setq lsp-java-configuration-runtimes '[(:name "JavaSE-17"
-                                         :path "/Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home"
-                                         :default t)])
-
-;; Disable the LSP formatter universally
-;;(setq +format-with-lsp nil)
-
-;;(add-hook 'java-mode-hook #'format-all-mode)
-
-;; Ensure clay is loaded
-(after! clojure-mode
-  (require 'clay))
-
-;; Keybindings for Clay functions
-(map! :map clojure-mode-map
-      :localleader
-      (:prefix ("l" "clay")
-        :desc "Start Clay" "s" #'clay-start
-        :desc "Make HTML from namespace" "h" #'clay-make-ns-html
-        :desc "Make Quarto HTML from namespace" "q" #'clay-make-ns-quarto-html
-        :desc "Make Reveal.js slideshow" "r" #'clay-make-ns-quarto-revealjs
-        :desc "Make last sexp" "l" #'clay-make-last-sexp
-        :desc "Make defun at point" "f" #'clay-make-defun-at-point))
-
-;; [Developing Metabase with Emacs](https://www.metabase.com/docs/latest/developers-guide/emacs)
-(setq custom-file (concat user-emacs-directory ".custom.el")) ; tell Customize to save customizations to ~/.emacs.d/.custom.el
-(ignore-errors                                                ; load customizations from ~/.emacs.d/.custom.el
-  (load-file custom-file))
-
-;; eww
-;; ignore popup for eww
-(set-popup-rule! "^\\*eww" :ignore t) ; Prevent EWW from being treated as a popup
-
-;; sqlformat on save
-(use-package sqlformat
-  :commands (sqlformat sqlformat-buffer sqlformat-region)
-  :hook (sql-mode sqlformat-on-save-mode)
-  :init
-  (setq sqlformat-command 'pgformatter
-        sqlformat-args '("-s2" "-g" "-u1")))
-
-;; duckdb
-
-;;;###autoload
-(defun sql-duckdb (&optional buffer)
-  "Run duckdb as an inferior process.
-
-
-If buffer `*SQL*' exists but no process is running, make a new process.
-If buffer exists and a process is running, just switch to buffer
-`*SQL*'.
-
-Interpreter used comes from variable `sql-duckdb-program'.  Login uses
-the variables `sql-user', `sql-password', `sql-database', and
-`sql-server' as defaults, if set.  Additional command line parameters
-can be stored in the list `sql-duckdb-options'.
-
-The buffer is put in SQL interactive mode, giving commands for sending
-input.  See `sql-interactive-mode'.
-
-To set the buffer name directly, use \\[universal-argument]
-before \\[sql-duckdb].  Once session has started,
-\\[sql-rename-buffer] can be called separately to rename the
-buffer.
-
-To specify a coding system for converting non-ASCII characters
-in the input and output to the process, use \\[universal-coding-system-argument]
-before \\[sql-duckdb].  You can also specify this with \\[set-buffer-process-coding-system]
-in the SQL buffer, after you start the process.
-The default comes from `process-coding-system-alist' and
-`default-process-coding-system'.
-
-\(Type \\[describe-mode] in the SQL buffer for a list of commands.)"
-  (interactive "P")
-  (sql-product-interactive 'duckdb buffer))
-(defun sql-duckdb-completion-object (sqlbuf _schema)
-  (sql-redirect-value sqlbuf "show tables" "^\\(\\sw\\(?:\\sw\\|\\s_\\)*\\)," 1))
-
-(defcustom sql-duckdb-program (or (executable-find "duckdb")
-                                  "duckdb")
-  "Command to start duckdub.
-
-Starts `sql-interactive-mode' after doing some setup."
-  :type 'file
-  :group 'SQL)
-
-(defcustom sql-duckdb-options nil
-  "List of additional options for `sql-duckdb-program'."
-  :type '(repeat string)
-  :version "20.8" ;; FIXME: What is this?
-  :group 'SQL)
-
-(defcustom sql-duckdb-login-params '((database :file nil
-                                               :must-match confirm))
-  "List of login parameters needed to connect to duckdb."
-  :type 'sql-login-params
-  :version "26.1"
-  :group 'SQL)
-
-(with-eval-after-load 'sql
-  (add-to-list 'sql-product-alist
-               '(duckdb
-                 :name "duckdb"
-                 :free-software nil
-                 :font-lock sql-mode-sqlite-font-lock-keywords  ;; Use sqlite for now
-                 :sqli-program sql-duckdb-program
-                 :sqli-options sql-duckdb-options
-                 :sqli-login sql-duckdb-login-params
-                 :sqli-comint-func sql-comint-sqlite
-                 :list-all "show tables"
-                 :list-table "describe %s"
-                 :completion-object sql-duckdb-completion-object
-                 :prompt-regexp "^D "
-                 :prompt-length 2
-                 :prompt-cont-regexp "^> ")))
-
-(require 'ob-sql)
-
-(defvar org-babel-default-header-args:duckdb
-  '((:results . "output") (:exports . "both"))
-  "Default arguments for evaluating a DuckDB code block.")
-
-(defun org-babel-execute:duckdb (body params)
-  "Execute a block of DuckDB SQL code with org-babel.
-This function is called by `org-babel-execute-src-block'."
-  (let* ((processed-params (org-babel-process-params params))
-         (db (or (cdr (assq :db processed-params))
-                 (error "No database specified")))
-         (sqlcmd (or (executable-find sql-duckdb-program)
-                     (error "DuckDB executable not found")))
-         (query body))
-    (with-temp-buffer
-      (call-process sqlcmd nil t nil db "-batch" "-csv" query)
-      ;; Capture and return the buffer's contents.
-      (buffer-string))))
-
-(add-to-list 'org-babel-tangle-lang-exts '("duckdb" . "sql"))
-
-(with-eval-after-load 'sql
-  (add-to-list 'org-babel-load-languages '(duckdb . t)))
-
-;; Add duckdb to the list of org-babel languages with SQL syntax highlighting
-(add-to-list 'org-src-lang-modes '("duckdb" . sql))
-
-(defun my-dabbrev-syntax-fix ()
-  "Modify syntax table for dabbrev to treat / as a separator."
-  (modify-syntax-entry ?/ " "))
-(add-hook 'org-mode-hook 'my-dabbrev-syntax-fix) ; Apply to text-mode
-
-;;
-(after! typescript-mode
-  (setq typescript-indent-level 2))
-
-(after! web-mode
-  (setq web-mode-markup-indent-offset 2)  ;; For JSX/TSX HTML-like syntax
-  (setq web-mode-css-indent-offset 2)     ;; For CSS inside JSX/TSX
-  (setq web-mode-code-indent-offset 2))  ;; For JS/TS code inside JSX/TSX
+  :config
+  (add-to-list 'copilot-indentation-alist '(prog-mode 2))
+  (add-to-list 'copilot-indentation-alist '(org-mode 2))
+  (add-to-list 'copilot-indentation-alist '(text-mode 2))
+  (add-to-list 'copilot-indentation-alist '(closure-mode 2))
+  (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2)))
--- init.el
+++ init.el
@@ -21,11 +21,12 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       (company +childframe)           ; the ultimate code completion backend
+       ;;company           ; the ultimate code completion backend
+       (corfu +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       (vertico +icons)           ; the search engine of the future
+       vertico           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
@@ -34,7 +35,6 @@
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;;hydra
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
@@ -50,7 +50,7 @@
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
-       zen               ; distraction-free coding or writing
+       ;;zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
@@ -69,7 +69,8 @@
        :emacs
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
+       ;;eww               ; the internet is gross
+       ibuffer           ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
@@ -87,13 +88,13 @@
        :tools
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
-       (debugger +lsp)          ; FIXME stepping through code, to help you add bugs
+       ;;collab            ; buffers with friends
+       ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        docker
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
@@ -101,22 +102,20 @@
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
-       ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;;tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
+       (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
        ;;tty               ; improve the terminal Emacs experience
 
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        ;;(cc +lsp)         ; C > C++ == 1
-       (clojure +lsp)           ; java with a lisp
+       (clojure +lsp +tree-sitter)           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
@@ -136,14 +135,13 @@
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
-       go         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
        ;;(haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
-       (java +lsp)       ; the poster child for carpal tunnel syndrome
-       (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
+       (java +lsp +tree-sitter)       ; the poster child for carpal tunnel syndrome
+       (javascript +lsp +tree-sitter)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
@@ -152,20 +150,21 @@
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       nix               ; I hereby declare "nix geht mehr!"
+       ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam2)               ; organize your plain life in plain text
+       org               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
+       ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
-       (python +lsp +pyright +pyenv)            ; beautiful is better than ugly
+       ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       (rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
@@ -175,7 +174,7 @@
        ;;terra             ; Earth and Moon in alignment for performance.
        web               ; the tubes
        yaml              ; JSON, but readable
-       zig               ; C, but simpler
+       ;;zig               ; C, but simpler
 
        :email
        ;;(mu4e +org +gmail)
@@ -188,7 +187,6 @@
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
-       ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
        ;;literate
